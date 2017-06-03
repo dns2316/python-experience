4112f0facabba1e05ae83cbcbdb52ffd7f7032d4
dns2316 <dns2316@gmail.com>
2017-06-03 22:30:34 +0300

    edit admin page

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,  {'fields':['article_title']}),
+        ('Body article info', {'fields':['pub_date','article_text']}),
+    ]
+
+admin.site.register(Article, ArticleAdmin)
